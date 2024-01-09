015ed505832404b8f62d7c89cfa36aa82f31e3b4
Paul <pgr@thepeaklab.com>
2024-01-09 17:07:52 +0100

    feat: :tada: setup package with typescript.

---
+++ index.ts
@@ -0,0 +1,13 @@
+/**
+ * Context: fields...
+ *
+ * Zod schema -> text, number, email etc.
+ *
+ * renderForm(schema, options) -> controls
+ *   controls.name
+ *   controls.email
+ *   ...
+ */
+export const example = () => {
+  console.log("Hello world");
+};
