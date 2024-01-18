527e6ae19d1fc1391a8f3f2a703948af3d3b7544
Paul <pgr@thepeaklab.com>
2024-01-18 23:44:24 +0100

    feat: support boolean, enum and effects

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,46 @@
 import { z } from "zod";
 import { renderForm } from "./form-renderer";
 
-// const example = z.object({
-//   title: z.enum(["Mr", "Mrs", "Miss", "Dr", "Prof"]),
-//   name: z.string(),
-//   email: z.string().email(),
-//   web: z.string().url(),
-//   count: z.number(),
-//   birth: z.date(),
-//   // file
-//   // textarea
-//   // radio
-//   // checkbox
-// });
-
 export const App = () => {
   const schema = z.object({
+    title: z.enum(["Mr", "Mrs", "Miss", "Dr", "Prof"]),
     name: z.string(),
     email: z.string().email(),
+    web: z.string().url(),
     age: z.number(),
+    birthDate: z.date(),
+    avatar: z.custom<File>().refine((file) => file.type.startsWith("image/")),
     accept: z.boolean(),
   });
 
-  const { controls } = renderForm(schema);
+  const schemaWithEffects = schema.refine((data) => {
+    return data.age > 18;
+  });
+
+  const { controls } = renderForm(schemaWithEffects);
 
   return (
     <div style={{ display: "flex", flexDirection: "column", rowGap: "2rem" }}>
       <b>Hello zod-form-renderer</b>
 
-      {controls.name({ label: "Name Label" })}
-      {controls.email({ label: "Email Label" })}
-      {controls.age()}
-      {controls.accept()}
+      {/*
+        DX verbessern:
+        Return React components statt functions. Params incl. overwrite, file.
+      */}
+      {controls.title({
+        label: "Title",
+        options: [
+          { value: "Mr", label: "Mr." },
+          { value: "Mrs", label: "Mrs." },
+          { value: "Miss", label: "Miss." },
+          { value: "Dr", label: "Dr." },
+          { value: "Prof", label: "Prof." },
+        ],
+      })}
+      {controls.name({ label: "Name" })}
+      {controls.email({ label: "Email" })}
+      {controls.age({ label: "Age" })}
+      {controls.accept({ label: "Accept privacy policy" })}
     </div>
   );
 };
--- src/form-renderer.tsx
+++ src/form-renderer.tsx
@@ -1,68 +1,120 @@
-import { ComponentPropsWithRef, ComponentPropsWithoutRef } from "react";
+import { ComponentPropsWithRef } from "react";
 import { z } from "zod";
 
 const NullRenderer = () => null;
 
-type InputRendererOptions = ComponentPropsWithRef<"input"> & {
+type InputProps = ComponentPropsWithRef<"input">;
+
+type InputRendererOptions = InputProps & {
   label: string;
 };
 
-const InputRenderer = (options: InputRendererOptions) => {
+const InputRenderer =
+  (renderOptions: InputProps) => (userOptions: InputRendererOptions) => {
+    const options = { ...renderOptions, ...userOptions };
+
+    return (
+      <div>
+        <label htmlFor={options.name}>{options.label}: </label>
+        <br />
+        <input {...options} />
+      </div>
+    );
+  };
+
+type CheckboxRendererOptions = InputProps & {
+  label: string;
+};
+
+const CheckboxRenderer = (userOptions: CheckboxRendererOptions) => {
+  const options = { type: "checkbox", ...userOptions };
+
+  return (
+    <label htmlFor={options.name}>
+      <input {...options} />
+      {options.label}
+    </label>
+  );
+};
+
+type SelectOptionValue = ComponentPropsWithRef<"option">["value"];
+
+type SelectRendererOptions<TValue extends SelectOptionValue> =
+  ComponentPropsWithRef<"select"> & {
+    label: string;
+    options: { value: TValue; label: string }[];
+  };
+
+const SelectRenderer = <TValue extends SelectOptionValue>(
+  userOptions: SelectRendererOptions<TValue>
+) => {
   return (
     <div>
-      <label htmlFor={options.name}>{options.label}: </label>
+      <label htmlFor={userOptions.name}>{userOptions.label}: </label>
       <br />
-      <input {...options} />
+      <select {...userOptions}>
+        {userOptions.options.map((option) => (
+          <option key={option.value?.toString()} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
     </div>
   );
 };
 
+type TRenderer<TValue> = TValue extends z.ZodEnum<
+  infer TEnum extends [string, ...string[]]
+>
+  ? typeof SelectRenderer<TEnum[number]>
+  : TValue extends z.ZodType<string>
+  ? ReturnType<typeof InputRenderer>
+  : TValue extends z.ZodType<number>
+  ? ReturnType<typeof InputRenderer>
+  : TValue extends z.ZodType<boolean>
+  ? typeof CheckboxRenderer
+  : typeof NullRenderer;
+// datepicker
+
 export function renderForm<
   TShape extends z.ZodRawShape,
   TKey extends keyof TShape
->(schema: z.ZodObject<TShape>) {
-  console.log(schema.shape, Object.entries(schema.shape));
+>(schema: z.ZodObject<TShape> | z.ZodEffects<z.ZodObject<TShape>>) {
+  const shape = isZodEffects(schema) ? schema._def.schema.shape : schema.shape;
 
-  type TRenderer<TValue> = TValue extends z.ZodType<string>
-    ? typeof InputRenderer
-    : typeof NullRenderer;
-
-  const controls = Object.entries(schema.shape).reduce(
-    (ctrls, [key, value]) => {
-      return { ...ctrls, [key]: findRenderer(value) };
-    },
-    {} as { [K in TKey]: TRenderer<TShape[K]> }
-  );
+  const controls = Object.entries(shape).reduce((ctrls, [key, value]) => {
+    return { ...ctrls, [key]: findRenderer(value) };
+  }, {} as { [K in TKey]: TRenderer<TShape[K]> });
 
   return { controls };
 }
 
 const findRenderer = <TValue extends z.ZodTypeAny>(value: TValue) => {
-  const type = getInputType(value);
-
-  if (type) {
-    return InputRenderer;
+  if (isEnum(value)) {
+    return SelectRenderer;
   }
 
-  return NullRenderer;
-};
-
-// TODO: Make work with effects: https://zod.dev/?id=zodtype-with-zodeffects
-
-type InputControlType = ComponentPropsWithoutRef<"input">["type"];
-
-function getInputType(value: z.ZodTypeAny): InputControlType | null {
   if (isString(value)) {
-    if (value.isURL) return "url";
-    if (value.isEmail) return "email";
-    return "text";
+    const type = value.isEmail ? "email" : value.isURL ? "url" : "text";
+    return InputRenderer({ type });
   }
 
   if (isNumber(value)) {
-    return "number";
+    return InputRenderer({ type: "number" });
   }
 
-  return null;
+  if (isBoolean(value)) {
+    return CheckboxRenderer;
+  }
+
+  console.log("No renderer found for", value._def.typeName);
+  return NullRenderer;
+};
+
+function isZodEffects<T extends z.ZodTypeAny>(
+  value: z.ZodTypeAny
+): value is z.ZodEffects<T> {
+  return value._def.typeName === "ZodEffects";
 }
 
 function isString(value: z.ZodTypeAny): value is z.ZodString {
@@ -73,12 +125,12 @@ function isNumber(value: z.ZodTypeAny): value is z.ZodNumber {
   return value._def.typeName === z.number()._def.typeName;
 }
 
-// function isBoolean(value: z.ZodTypeAny): value is z.ZodBoolean {
-//   return value._def.typeName === z.boolean()._def.typeName;
-// }
+function isBoolean(value: z.ZodTypeAny): value is z.ZodBoolean {
+  return value._def.typeName === z.boolean()._def.typeName;
+}
 
-// function isEnum<T extends [string, ...string[]]>(
-//   value: z.ZodTypeAny
-// ): value is z.ZodEnum<T> {
-//   return value._def.typeName === z.enum([""])._def.typeName;
-// }
+function isEnum<T extends [string, ...string[]]>(
+  value: z.ZodTypeAny
+): value is z.ZodEnum<T> {
+  return value._def.typeName === z.enum([""])._def.typeName;
+}
