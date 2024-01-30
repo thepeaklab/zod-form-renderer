c51dc9bedbccc0030bac830e713ca5b5a91f6433
Paul <pgr@thepeaklab.com>
2024-01-30 10:41:39 +0100

    fix: :package: dependencies setup.

--- test/form-renderer.spec.ts
+++ test/form-renderer.spec.ts
@@ -1,6 +1,17 @@
 import { expect, test } from "vitest";
-import { transform } from "../src/form-renderer";
+import { z } from "zod";
+import { useFormRenderer } from "../src/lib/use-form-renderer";
 
-test("returns object with name", () => {
-  expect(transform().name).toBe("Zod");
+test("returns form fields for each schema property", () => {
+  const schema = z.object({
+    title: z.enum(["Mr", "Mrs", "Miss", "Dr", "Prof"]),
+    name: z.string(),
+    accept: z.boolean(),
+  });
+
+  const fields = useFormRenderer(schema);
+
+  expect(fields.Title).toBeDefined();
+  expect(fields.Name).toBeDefined();
+  expect(fields.Accept).toBeDefined();
 });
