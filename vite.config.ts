a2c866218d40aea3f59ee84723e08cae911abc11
Paul <pgr@thepeaklab.com>
2024-07-11 10:13:21 +0200

    build: configure vite library build

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 /// <reference types="vitest" />
+import typescript from "@rollup/plugin-typescript";
 import react from "@vitejs/plugin-react";
+import * as path from "path";
+import { typescriptPaths } from "rollup-plugin-typescript-paths";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
@@ -13,6 +16,31 @@ export default defineConfig({
     alias: {
       "@src": "/src",
       "@test": "/test",
+    },
+  },
+  build: {
+    manifest: true,
+    minify: true,
+    reportCompressedSize: true,
+    emptyOutDir: true,
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      fileName: "index",
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: [],
+      plugins: [
+        typescriptPaths({
+          preserveExtensions: true,
+        }),
+        typescript({
+          sourceMap: false,
+          declaration: true,
+          outDir: "dist",
+          exclude: ["test/**/*", "playground/**/*"],
+        }),
+      ],
     },
   },
 });
